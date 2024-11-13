b7e9a130ad75ce3f051a0fbe19d9ca3f0e616da4
icefox-saber <728862050a@gmail.com>
2024-11-13 19:42:42 +0800

    select

---
+++ Selection/select.cpp
@@ -0,0 +1,9 @@
+#include "select.h"
+#include <iostream>
+
+int main()
+{
+    std::vector<int> v={4,10,6,9,2,9,7,8,4};
+
+    std::cout<<"select the 3th smallest:"<<select(v,4);
+}
--- Selection/select.h
+++ Selection/select.h
@@ -34,6 +34,37 @@ int random_partition(std::vector<T> &vec, int l, int r)
     return partition(vec,l,r);
 }
 
-template <
+template <typename T>
+T select(std::vector<T> & vec,int i,int l,int r)
+{
+    assert(i<=r-l+1);
+    
+    if(l==r)
+        return vec[l];
+    
+    int p=random_partition(vec,l,r);
+    if (i == p-l+1)
+    {
+        return vec[p];
+    }
+
+    else if(i>p-l+1)
+    {
+        return select(vec,i-p+l-1,p+1,r);
+    }
+
+    else
+    {
+        return select(vec,i,l,p-1);
+    }
+    
+}
+
+template <typename T>
+T select(const std::vector<T> & vec,int i)
+{
+    std::vector<T> copy=vec;
+    return select(copy,i,0,copy.size()-1);
+}
 
 #endif
