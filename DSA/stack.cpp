6b6b815d210b440c692d60dc48ce30711cdeb10a
icefox-saber <728862050a@gmail.com>
2024-11-25 20:55:42 +0800

    format

--- DSA/stack.cpp
+++ DSA/stack.cpp
@@ -1,26 +1,18 @@
 #include "stack.h"
 #include <iostream>
 
-int main()
-{
-    CLRS::stack<int> stk;
-    stk.push(30);
-    std::cout<<"after push 30:"<<std::endl
-    <<stk.top()<<std::endl;
-    stk.push(20);
-    std::cout<<"after push 20:"<<std::endl
-    <<stk.top()<<std::endl;
-    stk.push(10);
-    std::cout<<"after push 10:"<<std::endl
-    <<stk.top()<<std::endl;
-    stk.pop();
-    std::cout<<"after pop 10:"<<std::endl
-    <<stk.top()<<std::endl;
-    stk.pop();
-    std::cout<<"after pop 20:"<<std::endl
-    <<stk.top()<<std::endl;
-    stk.pop();
-    std::cout<<"after pop 30:"<<std::endl
-    <<stk.top()<<std::endl;
-
+int main() {
+  CLRS::stack<int> stk;
+  stk.push(30);
+  std::cout << "after push 30:" << std::endl << stk.top() << std::endl;
+  stk.push(20);
+  std::cout << "after push 20:" << std::endl << stk.top() << std::endl;
+  stk.push(10);
+  std::cout << "after push 10:" << std::endl << stk.top() << std::endl;
+  stk.pop();
+  std::cout << "after pop 10:" << std::endl << stk.top() << std::endl;
+  stk.pop();
+  std::cout << "after pop 20:" << std::endl << stk.top() << std::endl;
+  stk.pop();
+  std::cout << "after pop 30:" << std::endl << stk.top() << std::endl;
 }
--- DSA/stack.h
+++ DSA/stack.h
@@ -23,22 +23,21 @@ template <typename T> stack<T>::stack() {}
 
 template <typename T> stack<T>::~stack() {}
 
-template <typename T> bool stack<T>::empty()
-{
-  return list.empty();
-}
+template <typename T> bool stack<T>::empty() { return list.empty(); }
 
 /// @brief push a value into stack
-/// @tparam T 
+/// @tparam T
 /// @param x the value to be pushed
 template <typename T> void stack<T>::push(const T &x) { list.prepend(x); }
 
 /// @brief pop the top element out, no return
-/// @tparam T 
-template <typename T> void stack<T>::pop() { list.remove(list.gethead()->next); }
+/// @tparam T
+template <typename T> void stack<T>::pop() {
+  list.remove(list.gethead()->next);
+}
 
 /// @brief get the top element
-/// @tparam T 
+/// @tparam T
 /// @return return the top element's key
 template <typename T> T stack<T>::top() { return list.gethead()->next->key; }
 
