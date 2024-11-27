d4329c226d90589e5fd5a40394e21177c40ce0c1
icefox-saber <728862050a@gmail.com>
2024-11-27 16:31:46 +0800

    change order begin at 0, fix bug

--- Selection/select5.h
+++ Selection/select5.h
@@ -6,17 +6,9 @@
 #include <assert.h>
 #include <cmath>
 #include <vector>
+#include "../Sort/quicksort.h"
 
 namespace CLRS {
-// 可变参数模板排序函数，直接接受多个引用参数
-template <typename T, typename... Args> void sort(T &a, T &b, Args &...args) {
-  // 使用 std::array 存储引用的指针
-  std::array<T *, sizeof...(args) + 2> elements = {&a, &b, &args...};
-
-  // 使用标准库排序，并解引用进行比较
-  std::sort(elements.begin(), elements.end(),
-            [](T *lhs, T *rhs) { return *lhs < *rhs; });
-}
 
 template <typename T> int min(const std::vector<T> &vec) {
   int p = 0;
@@ -44,20 +36,6 @@ int min(const std::vector<T> &vec, std::size_t l, std::size_t r) {
   return p;
 }
 
-template <typename T> int partition(std::vector<T> &vec, int l, int r) {
-  T pivot = vec[r];
-  int i = l, j;
-  for (j = l; j < r; j++) {
-    if (vec[j] <= pivot) {
-      std::swap(vec[i], vec[j]);
-      i++;
-    }
-  }
-  assert(j == r);
-  std::swap(vec[i], vec[j]);
-  return i;
-}
-
 template <typename T> int partition(std::vector<T> &vec, int l, int r, int p) {
   std::swap(vec[r], vec[p]);
   return partition(vec, l, r);
@@ -66,17 +44,17 @@ template <typename T> int partition(std::vector<T> &vec, int l, int r, int p) {
 /// @brief select the ith element in vec
 /// @tparam T element type
 /// @param vec element vec
-/// @param i the order of the element
+/// @param i the order of the element(begin at 0)
 /// @param l left bounds index of vec
 /// @param r right bounds index of vec
 /// @return the index of the ith element
 template <typename T> int select5(std::vector<T> &vec, int i, int l, int r) {
-  assert(i <= r - l + 1);
+  assert(i < r - l + 1);
   while (((r - l + 1) % 5) != 0) {
     int mindex = min(vec, l, r);
     std::swap(vec[mindex], vec[l]);
 
-    if (i == 1) {
+    if (i == 0) {
       return l;
     }
     l++;
@@ -87,18 +65,24 @@ template <typename T> int select5(std::vector<T> &vec, int i, int l, int r) {
 
   int g = (r - l + 1) / 5;
 
-  for (int j = l; j < r; j += 5) {
-    sort(vec[j], vec[j + g], vec[j + 2 * g], vec[j + 3 * g], vec[j + 4 * g]);
+  for (int j = l, k =0; k < g; j ++,k++) {
+    std::vector<T> vec_5={vec[j], vec[j + g], vec[j + 2 * g], vec[j + 3 * g], vec[j + 4 * g]};
+    quickSort(vec_5);
+    for (int i = 0; i < 5; i++)
+    {
+      vec[j+i*g]=vec_5[i];
+    }
+    
   }
 
-  int x = select5(vec, std::ceil(g / 2.0), l + 2 * g, l + 3 * g - 1);
+  int x = select5(vec, g / 2, l + 2 * g, l + 3 * g - 1);
 
   int p = partition(vec, l, r, x);
-  if (i == p - l + 1) {
+  if (i == p - l) {
     return p;
   }
 
-  else if (i > p - l + 1) {
+  else if (i > p - l) {
     return select5(vec, i - p + l - 1, p + 1, r);
   }
 
@@ -109,8 +93,13 @@ template <typename T> int select5(std::vector<T> &vec, int i, int l, int r) {
   return -1;
 }
 
+/// @brief select the ith element in vec
+/// @tparam T element type
+/// @param vec element vec
+/// @param i the order of the element(begin at 0)
+/// @return the ith element
 template <typename T> T select5(const std::vector<T> &vec, std::size_t i) {
-  assert(i <= vec.size());
+  assert(i < vec.size());
   std::vector<T> copy = vec;
   int x = select5(copy, i, 0, copy.size() - 1);
   return copy[x];
